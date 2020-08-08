f66aac9d8462be132adbe0eb68925e27e6c66ffb
desaijay315 <desaijay315@gmail.com>
2020-08-08 16:04:04 +0530

    elasticsearch-react-reactivesearch

---
+++ src/App.js
@@ -0,0 +1,68 @@
+import React, { Component } from 'react';
+import { ReactiveBase, DataSearch } from '@appbaseio/reactivesearch';
+
+import Header from './components/Header';
+import Results from './components/Results';
+
+import theme from './theme';
+import './App.css';
+
+class App extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			currentTopics: [],
+		};
+	}
+
+	setTopics = (currentTopics) => {
+		this.setState({
+			currentTopics: currentTopics || [],
+		});
+	}
+
+	toggleTopic = (topic) => {
+		const { currentTopics } = this.state;
+		const nextState = currentTopics.includes(topic)
+			? currentTopics.filter(item => item !== topic)
+			: currentTopics.concat(topic);
+		this.setState({
+			currentTopics: nextState,
+		});
+	}
+
+	render() {
+		return (
+			<section className="container">
+				<ReactiveBase
+					app="gitxplore-app"
+					credentials="4oaS4Srzi:f6966181-1eb4-443c-8e0e-b7f38e7bc316"
+					type="gitxplore-latest"
+					theme={theme}
+				>
+					<div className="flex row-reverse app-container">
+						<Header currentTopics={this.state.currentTopics} setTopics={this.setTopics} />
+						<div className="results-container">
+							<DataSearch
+								componentId="repo"
+								filterLabel="Search"
+								dataField={['name', 'description', 'name.raw', 'fullname', 'owner', 'topics']}
+								placeholder="Search Repos"
+								iconPosition="left"
+								autosuggest={false}
+								URLParams
+								className="data-search-container results-container"
+								innerClass={{
+									input: 'search-input',
+								}}
+							/>
+							<Results currentTopics={this.state.currentTopics} toggleTopic={this.toggleTopic} />
+						</div>
+					</div>
+				</ReactiveBase>
+			</section>
+		);
+	}
+}
+
+export default App;
---
+++ src/theme.js
@@ -0,0 +1,12 @@
+const theme = {
+	typography: {
+		fontFamily: 'Raleway, Helvetica, sans-serif',
+	},
+	colors: {
+		primaryColor: '#008000',
+		titleColor: 'white'
+	},
+	secondaryColor: 'mediumseagreen',
+};
+
+export default theme;
